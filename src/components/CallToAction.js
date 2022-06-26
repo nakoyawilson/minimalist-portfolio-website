86a3c7a784113ccfba1897bf8e26e3ff487d8eff
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-06-26 18:59:08 -0400

    Add hover/active states

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 import "./CallToAction.css";
 
-const CallToAction = ({ scrollToTop }) => {
+const CallToAction = () => {
+  const scrollToTop = useOutletContext();
+
   return (
     <section className="cta">
       <h2 className="section-heading">
         Interested in doing a project together?
       </h2>
-      <Link to="contact" onClick={scrollToTop} className="btn-secondary">
+      <Link to="/contact" onClick={scrollToTop} className="btn-secondary">
         Contact Me
       </Link>
     </section>
--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,7 +1,20 @@
+import { useForm } from "react-hook-form";
 import SocialIcons from "../components/SocialIcons";
 import "./ContactMe.css";
 
 const ContactMe = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    document
+      .querySelectorAll(".form-input")
+      .forEach((formInput) => (formInput.value = ""));
+  };
+
   return (
     <main className="contact container">
       <section className="get-in-touch">
@@ -22,32 +35,60 @@ const ContactMe = () => {
       </section>
       <section className="contact-me">
         <h2 className="section-heading">Contact Me</h2>
-        <form className="contact-form">
-          <label htmlFor="full-name" className="input-label">
-            Name
-          </label>
-          <input
-            type="text"
-            id="full-name"
-            placeholder="Jane Appleseed"
-            className="form-input"
-          />
-          <label htmlFor="email-address" className="input-label">
-            Email Address
-          </label>
-          <input
-            type="email"
-            placeholder="email@example.com"
-            className="form-input"
-          />
-          <label htmlFor="message" className="input-label">
-            Message
-          </label>
-          <textarea
-            id="message"
-            placeholder="How can I help?"
-            className="form-input message-input"
-          ></textarea>
+        <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-wrapper">
+            <label htmlFor="full-name" className="input-label">
+              Name
+            </label>
+            <input
+              type="text"
+              id="full-name"
+              placeholder="Jane Appleseed"
+              className={`form-input${errors.fullName ? " input-error" : ""}`}
+              {...register("fullName", { required: true })}
+            />
+            {errors.fullName?.type === "required" && (
+              <p className="error-msg">This field is required</p>
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="email-address" className="input-label">
+              Email Address
+            </label>
+            <input
+              type="email"
+              placeholder="email@example.com"
+              className={`form-input${
+                errors.emailAddress ? " input-error" : ""
+              }`}
+              {...register("emailAddress", {
+                required: true,
+                pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+              })}
+            />
+            {errors.emailAddress?.type === "required" && (
+              <p className="error-msg">This field is required</p>
+            )}
+            {errors.emailAddress?.type === "pattern" && (
+              <p className="error-msg">Please use a valid email address</p>
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="message" className="input-label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              placeholder="How can I help?"
+              className={`form-input message-input${
+                errors.fullName ? " input-error" : ""
+              }`}
+              {...register("yourMessage", { required: true })}
+            ></textarea>
+            {errors.yourMessage?.type === "required" && (
+              <p className="error-msg">This field is required</p>
+            )}
+          </div>
           <button className="send-btn">Send Message</button>
         </form>
       </section>
